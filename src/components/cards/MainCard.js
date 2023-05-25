a69cb35d5fe3f687c180f3645549d90924be4ebb
Jered Adkins <jeredkins@gmail.com>
2023-05-25 12:32:56 -0700

    attack card3!

--- src/components/cards/MainCard.js
+++ src/components/cards/MainCard.js
@@ -8,6 +8,7 @@ import {
   Card4Atom,
   Card5Atom,
 } from '../../data/Atoms'
+import MainCardStyles from './MainCardStyles'
 
 const cardAtoms = [
   Card0Atom,
@@ -20,22 +21,34 @@ const cardAtoms = [
 
 const MainCard = ({ card, index }) => {
   const [cardData, setCardData] = useAtom(cardAtoms[index])
+  const [card2Data, setCard2Data] = useAtom(Card2Atom)
 
   React.useEffect(() => {
     setCardData(card)
   }, [card, setCardData])
 
   const minusHp = () => {
-    setCardData({ ...cardData, hp: cardData.hp - 1 })
+    setCardData((prevCardData) => ({
+      ...prevCardData,
+      hp: (prevCardData.hp || 0) - 1,
+    }))
+  }
+
+  const minusHpCard2 = () => {
+    setCard2Data((prevCard2Data) => ({
+      ...prevCard2Data,
+      hp: (prevCard2Data.hp || 0) - 1,
+    }))
   }
 
   console.log(card, index)
   return (
-    <div>
+    <MainCardStyles>
       <p>{card.name}</p>
-      {cardData && <p>{cardData.hp}</p>}
+      {cardData && <p>{cardData.hp || 0}</p>}
       <button onClick={minusHp}>-1 hp</button>
-    </div>
+      <button onClick={minusHpCard2}>-1 hp for Card2Atom</button>
+    </MainCardStyles>
   )
 }
 
---
+++ src/components/cards/MainCardStyles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const MainCardStyles = styled.div``
+
+export default MainCardStyles
